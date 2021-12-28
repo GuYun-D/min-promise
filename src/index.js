6a2b94a00d0f3d270682c8b9415f518b92ec9ba5
GuYun-D <1946306647@qq.com>
2021-12-28 22:04:01 +0800

    then方法的简单实现

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import GYPromise from "./lib/promise.js";
 
 const promise = new GYPromise((resolve, reject) => {
-  resolve()
-  reject()
+  // resolve("我是resolve参数")
+  reject("我是reject参数")
 })
+
+promise.then(
+  res => {
+    console.log("res:", res);
+  },
+  err => {
+    console.log("err:", err);
+  }
+)
--- src/lib/promise.js
+++ src/lib/promise.js
@@ -11,23 +11,46 @@ class GYPromise {
     // executor 执行期间，promise的状态是pending的
     this.status = PROMISE_STATUS_PENDING
 
-    const resolve = () => {
+    // 保存resolve和reject参数
+    this.value = undefined
+    this.reason = undefined
+
+    const resolve = (value) => {
       // 不论是resolve和reject函数，只有当status === pending时才会执行
       if (this.status === PROMISE_STATUS_PENDING) {
         // 修改promise的状态为成功的
         this.status = PROMISE_STATUS_FULFILLED
-        console.log("resolve执行了");
+        queueMicrotask(() => {
+          // 保存参数
+          this.value = value
+          // 执行then传递的第一个回调函数
+          this.onFulfilled(this.value)
+        })
       }
     }
 
-    const reject = () => {
+    const reject = (reason) => {
       if (this.status === PROMISE_STATUS_PENDING) {
         this.status = PROMISE_STATUS_REJECTED
-        console.log("reject执行了");
+        queueMicrotask(() => {
+          this.reason = reason
+          // 执行then传递的第二个回调函数
+          this.onRejected(this.reason)
+        })
       }
     }
 
     executor(resolve, reject)
+  }
+
+  /**
+   * 实例方法 then
+   * @param {*} onFulfilled 成功调用函数
+   * @param {*} onRejected 拒绝调用函数
+   */
+  then(onFulfilled, onRejected) {
+    this.onFulfilled = onFulfilled
+    this.onRejected = onRejected
   }
 }
 
